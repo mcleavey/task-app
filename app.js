5fce6267164c02c4dab326bbf26faaf1813050b7
Christine <mcleavey@gmail.com>
2017-04-19 20:43:20 +0000

    first complete working version

---
+++ app.js
@@ -0,0 +1,94 @@
+var express = require("express"),
+    app = express(),
+    mongoose = require("mongoose"),
+    logger = require("morgan"),
+    bodyParser = require("body-parser");
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.listen(process.env.PORT, process.env.IP, function() {
+console.log("Server started");
+});
+
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+mongoose.connect("mongodb://localhost/taskApp");
+
+var taskSchema = new mongoose.Schema({
+    name: String,
+    completed: Boolean,
+    importancelevel: Number,
+    urgencylevel: Number
+})
+
+var Task = mongoose.model("Task", taskSchema);
+
+
+
+
+app.get("/", function(req, res){
+    Task.find({}, function(err, tasks){
+    console.log("Retrieve:");
+    if (err){
+        console.log(err);
+    } else {
+        console.log("Show new page");
+          res.render("task", {tasks: tasks});
+    }
+ });
+});
+
+
+
+
+app.post("/act", function(req, res) {
+   if (req.body.action==="add") {
+      Task.create({name:req.body.name, completed: req.body.completed, importancelevel: req.body.importancelevel, urgencylevel: req.body.urgencylevel}, 
+         function(err){
+            console.log("Creating item in db:");
+            if (err){
+                console.log(err);
+            } else {
+                console.log("Success, now redirecting");
+                res.send("Successfully added");
+            }
+        });
+        } else if (req.body.action==="delete") {
+       // remove item from database
+            console.log("Delete: "+req.body.name);    
+            Task.findByIdAndRemove(req.body.id, function (err) {
+            console.log("Creating item in db:");
+            if (err){
+                console.log(err);
+            } else {
+                console.log("Success, now redirecting");
+                res.send("Successfully added");
+            }
+        });
+        
+        }  else if (req.body.action==="complete"){
+        // change completed status
+            Task.findById(req.body.id, function (err, task) {  
+    // Handle any possible database errors
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    task.completed = req.body.completed;
+                    // Save the updated document back to the database
+                    task.save(function (err, todo) {
+                        if (err) {
+                            res.status(500).send(err);
+                        }
+                    });  // end of task.save
+                }
+            });   // end of tast.findbyID
+  }   // end of if (to determine which action)
+  
+});   // end of app.post
+    
+// app.get("/:thing", function(req, res){
+//     res.render("home", {myVar: req.params.thing})
+// });
---
+++ public/ToDoGrid.js
@@ -0,0 +1,60 @@
+// Delete task
+$("ul").on("click", ".X", function(event){
+    console.log($(this).parent().text()+ " and ID: "+$(this).parent().find(".taskID").text());
+    $.post("/act", {
+       action: "delete",
+       name: $(this).parent().text(),
+       id: $(this).parent().find(".taskID").text()
+       }).done(
+            function(){
+                console.log("Finished delete");
+                location.href = location.href;
+        });
+
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+// Mark completed (grey, strike-through)
+$("ul").on("click", "li", function(){
+    var markComplete = !($(this).hasClass("completed"));
+    console.log("Mark as complete? "+markComplete);
+	$(this).toggleClass("completed");
+	$.post("/act", {
+	 action: "complete",
+	 completed: markComplete,
+	 id: $(this).find(".taskID").text()
+    });
+});
+
+
+// Add new task
+$("input").keypress(function(event){
+	if (event.which === 13) {
+		$(this).parent().parent().find("ul").append("<li><span class=\"X\"><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></span> "+$(this).val()+"</li>");
+		console.log("About to call .post for add");
+		$.post("/act",  {
+		    action: "add",
+            name: $(this).val(),
+            completed: false,
+            importancelevel: $(this).parent().find(".importance").text(),
+            urgencylevel:  $(this).parent().find(".urgency").text()
+            }).done(
+            function(){
+                console.log("Finished add");
+                location.href = location.href;
+        });
+       
+	}
+});
+
+$("ul").on("mouseover", "li", function(){
+	console.log("hover");
+	$(this).animate();
+})
+
+$(".fa-plus").click(function(){
+	$("input").slideToggle();
+});
